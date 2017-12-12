0cd71788586e48e0633f4bc07b963bfbe66c96ed
Josh Salwen <jnsalwen@yahoo.com>
2017-12-12 13:26:36 -0800

    add handler for outgoing slack messages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,75 @@
 import checkVersion from 'botpress-version-manager'
+import dashbotModule from 'dashbot'
+import path from 'path'
+import fs from 'fs'
 
 import axios from 'axios'
 
 let config = null
+let dashbot = {
+  facebook: null,
+  slack: null
+}
+
+const incomingMiddleware = (event, next) => {
+  switch (event.platform) {
+    case 'facebook':
+      let raw = event.raw
+      //event.bp.logger.debug('facebook raw', raw)
+      next()
+      break
+    case 'slack':
+      //event.bp.logger.debug('slack', event)
+      next()
+      break
+    default:
+      next()
+  }
+}
+
+const outgoingMiddleware = (event, next) => {
+  switch (event.platform) {
+    case 'facebook':
+      let raw = event.raw
+      // event.bp.logger.debug('facebook raw', raw)
+      next()
+      break
+    case 'slack':
+      if (!!dashbot.slack) {
+        let data = event.bp.slack && event.bp.slack.getData() || {}
+        const bot = {
+          id: data.self.id,
+          name: data.self.name
+        }
+        const team = {
+          id: data.team.id,
+          name: data.team.name
+        }
+        const reply = {
+          type: 'message',
+          text: event.text,
+          channel: event.raw.channelId
+        }
+        dashbot.slack.logOutgoing(bot, team, reply)
+        event.bp.logger.debug('slack - outgoing', bot, team, reply)
+      }
+      next()
+      break
+    default:
+      next()
+  }
+}
+
+const saveSettings = () => {
+  if (!!config.facebookApiKey) {
+    dashbot.slack = dashbotModule(config.facebookApiKey).facebook
+  }
+  if (!!config.slackApiKey) {
+    dashbot.slack = dashbotModule(config.slackApiKey).slack
+  }
+}
 
 module.exports = {
-
   config: {
     facebookApiKey: { type: 'string', env: 'DASHBOT_FACEBOOK_API_KEY' },
     slackApiKey: { type: 'string', env: 'DASHBOT_SLACK_API_KEY' }
@@ -14,7 +78,26 @@ module.exports = {
   init: async function(bp, configurator) {
     checkVersion(bp, __dirname)
 
+    bp.middlewares.register({
+      name: 'dashbot.incoming',
+      module: 'botpress-dashbot',
+      type: 'incoming',
+      handler: incomingMiddleware,
+      order: 10,
+      description: 'Send analytics data for incoming messages to Dashbot.'
+    })
+
+    bp.middlewares.register({
+      name: 'dashbot.outgoing',
+      module: 'botpress-dashbot',
+      type: 'outgoing',
+      handler: outgoingMiddleware,
+      order: 10,
+      description: 'Send analytics data for outgoing messages to Dashbot.'
+    })
+
     config = await configurator.loadAll()
+    saveSettings()
   },
 
   ready: async function(bp, configurator) {
@@ -27,6 +110,8 @@ module.exports = {
       const { facebookApiKey, slackApiKey } = req.body
       await configurator.saveAll({ facebookApiKey, slackApiKey })
       config = await configurator.loadAll()
+
+      saveSettings()
 
       res.sendStatus(200)
     })
